ecab72eca8fdf720e262979ec41d549fd093daff
Gael <gaelrodguez@hotmail.com>
2023-05-31 08:06:46 -0600

    Login working, mailer configured, contact me to set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
-  root "trainers#index"
+  root "static_pages#home"
+  
+  post "/create_user", to: "users#create"
+  get "/sign_up", to: "users#new"
+
+  post "/login", to: "sessions#create"
+  get "/login", to: "sessions#new"
+
+  get "/signin", to: "sessions#new"
   get "/pokemons/discover", to: "pokemons#discover"
   post "/pokemon/create/:order/:expected_name", to: "pokemons#createPokemon", as:'create-pokemon'
   get "/select_pokemon/:trainer_id", to: "pokemons#select_pokemon", as:'select_pokemon'
   post "/pokemons/add_pokemon/:trainer_id/:pokemon_id", to: "pokemons#add_pokemon", as:'add_pokemon'
   resources :pokemons
   resources :trainers
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+
+
+
+  delete "logout", to: "sessions#destroy"
+
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_164624) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_134835) do
+  create_table "currents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "guess_pokemons", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -37,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_164624) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "owns_pokemons", "pokemons"
